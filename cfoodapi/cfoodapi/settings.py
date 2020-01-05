1753ebacfec132cdb7f2d14ab65bb7d536d480b5
leoferreiralima <leoferreiralima@yahoo.com>
2020-01-05 14:55:01 -0300

    Database url config for heroku

--- cfoodapi/cfoodapi/settings.py
+++ cfoodapi/cfoodapi/settings.py
@@ -1,11 +1,8 @@
-
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import os
 from pathlib import Path
-import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/javascript", ".js", True)
+from django_heroku import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 try:
@@ -71,18 +68,7 @@ WSGI_APPLICATION = 'cfoodapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(engine="django.db.backends.postgresql"),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
